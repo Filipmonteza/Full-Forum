b8138eaf2b26915ab0883347017130dc990ffc45
filip Monteza <Filipnordic@gmail.com>
2026-03-18 13:39:13 +0100

    update ApplicationIdenityUser

--- src/FullForum-Infrastructure/Identity/ApplicationIdentityUser.cs
+++ src/FullForum-Infrastructure/Identity/ApplicationIdentityUser.cs
@@ -1,6 +1,19 @@
+using FullForum_Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
 namespace FullForum_Infrastructure.Identity;
 
-public class ApplicationIdentityUser
+public class ApplicationIdentityUser : IdentityUser<Guid>
 {
+    // Additional properties
+    public string DisplayName { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
     
+    // Navigation Properties
+    public ICollection<ForumThread> Threads { get; set; } = new List<ForumThread>();
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>(); 
 }
