1a12f7c27b90af180b6c8e75c13f630b2c070845
filip Monteza <Filipnordic@gmail.com>
2026-03-21 16:03:38 +0100

    feat: Added Interface for UserSerivce repository

--- src/FullForum-Application/Interfaces/IUserService.cs
+++ src/FullForum-Application/Interfaces/IUserService.cs
@@ -1,6 +1,14 @@
 ﻿namespace FullForum_Application.Interfaces;
 
+/// <summary>
+/// Service interface for retrieving user information by Id or DisplayName
+/// </summary>
 public interface IUserService
 {
+    /// <summary>
+    /// Return user with Id, otherwise null
+    /// </summary>
+    Task<bool> UserExistsAsync(Guid userId, CancellationToken cancellationToken = default);
     
+    Task<string?> GetDisplayNameAsync(Guid userId, CancellationToken cancellationToken = default);
 }
