de6e24ebfb1937d3005d184f9565aff066b6bdb2
Filip <Filipnordic@gmail.com>
2026-03-25 18:56:40 +0100

    feat persistance: added comment and thread configurations

--- src/FullForum-Infrastructure/Persistence/Configurations/CommentConfiguration.cs
+++ src/FullForum-Infrastructure/Persistence/Configurations/CommentConfiguration.cs
@@ -1,6 +1,38 @@
-﻿namespace FullForum_Infrastructure.Persistence.Configurations;
+﻿using FullForum_Domain.Entities;
+using FullForum_Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CommentConfiguration
+namespace FullForum_Infrastructure.Persistence.Configurations;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
 {
-    
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.CommentContent)
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.UpdatedAt);
+
+        // Comment → ApplicationUser (many-to-one)
+        builder.HasOne<ApplicationIdentityUser>()
+            .WithMany(x => x.Comments)
+            .HasForeignKey(x => x.ApplicationUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Comment → Thread (many-to-one)
+        builder.HasOne(x => x.Thread)
+            .WithMany(x => x.Comments)
+            .HasForeignKey(x => x.ThreadId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Self-referencing comments (reply tree)
+        builder.HasOne(x => x.ParentComment)
+            .WithMany(x => x.ChildComments)
+            .HasForeignKey(x => x.ParentCommentId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
--- src/FullForum-Infrastructure/Persistence/Configurations/ThreadConfiguration.cs
+++ src/FullForum-Infrastructure/Persistence/Configurations/ThreadConfiguration.cs
@@ -1,6 +1,39 @@
-﻿namespace FullForum_Infrastructure.Persistence.Configurations;
+﻿using FullForum_Domain.Entities;
+using FullForum_Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class ThreadConfiguration
+namespace FullForum_Infrastructure.Persistence.Configurations;
+
+public class ThreadConfiguration : IEntityTypeConfiguration<ForumThread>
 {
-    
+    public void Configure(EntityTypeBuilder<ForumThread> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.ThreadTitle)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder.Property(x => x.ThreadContent)
+            .IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.UpdatedAt);
+        
+        // Thread → Category (many-to-one)
+        builder.HasOne(x => x.Category)
+            .WithMany(x => x.Threads)
+            .HasForeignKey(x => x.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Thread → ApplicationUser (many-to-one)
+        builder.HasOne<ApplicationIdentityUser>()
+            .WithMany(x => x.Threads)
+            .HasForeignKey(x => x.ApplicationUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Thread → Comments (one-to-many)
+        builder.HasMany(x => x.Comments)
+            .WithOne(x => x.Thread)
+            .HasForeignKey(x => x.ThreadId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
