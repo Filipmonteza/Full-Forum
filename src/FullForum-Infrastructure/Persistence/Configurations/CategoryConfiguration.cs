2d4110bffd759e23cb9f50b7a75dddab56a2f377
Filip <Filipnordic@gmail.com>
2026-03-25 18:51:05 +0100

    feat persistance: add category entity configuration and fix: resolve nullable warnings for DbSet properties in ForumDbContext

--- src/FullForum-Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
+++ src/FullForum-Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -1,6 +1,20 @@
-﻿namespace FullForum_Infrastructure.Persistence.Configurations;
+﻿using FullForum_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CategoryConfiguration
+namespace FullForum_Infrastructure.Persistence.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
-    
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.CategoryName)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(x => x.CategoryDescription)
+            .HasMaxLength(500);
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.UpdatedAt);
+    }
 }
