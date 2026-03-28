4687b2e3ec91809909aee467fe4048ad0c2439b8
Filip <Filipnordic@gmail.com>
2026-03-28 11:08:39 +0100

    Added Thread Response and Request contracts

---
+++ src/FullForum-WebApi/Contracts/Threads/ThreadResponse/PagedThreadsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FullForum_WebApi.Contracts.Threads.ThreadResponse;
+
+// Api Response wrapping pginated list of threads, incuding total count
+public record PagedThreadsResponse(
+    List<ThreadListItemResponse> Items,
+    int TotalCount
+    );
+
+    
