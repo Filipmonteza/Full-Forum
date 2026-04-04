d42ae73072616ce6ab334c98aa960f3bc5b9b078
Filip <Filipnordic@gmail.com>
2026-04-04 12:08:29 +0200

    feat: add user activity response models

---
+++ src/FullForum-WebUi/Models/Users/Activity/UserActivityResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace FullForum_WebUi.Models.Users.Activity;
+
+// DTO for user activity, including threads and comments created by the user
+public sealed record UserActivityResponse(
+    List<UserThreadResponse> Threads,
+    List<UserCommentResponse> Comments);
