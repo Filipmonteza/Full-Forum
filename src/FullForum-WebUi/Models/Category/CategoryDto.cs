01b5800a05a52cce6dd2ce464ae061916e65c50b
Filip <Filipnordic@gmail.com>
2026-04-04 12:04:54 +0200

    feat: add CategoryDto for forum categories

---
+++ src/FullForum-WebUi/Models/Category/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FullForum_WebUi.Models.Category;
+
+/// <summary>
+/// Data Transfer Object (DTO) for representing a forum category.
+/// This DTO is used to transfer category data between the server and client.
+/// </summary>
+public sealed record CategoryDto(
+    Guid Id,
+    string CategoryName,
+    string CategoryDescription
+);
